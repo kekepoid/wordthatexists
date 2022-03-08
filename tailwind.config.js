2512e5d824292cdb20fac3d759afdc73a0af9ab1
kekepoid <101209551+kekepoid@users.noreply.github.com>
2022-03-08 20:39:00 +0200

    site added

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    content: [
+        "./index.html",
+        "./src/**/*.{vue,js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            fontSize: {
+                'xxs': '0.5rem',
+                '10xl': '9rem',
+                '11xl': '10rem',
+                '12xl': '11rem',
+                '13xl': '12rem',
+                '14xl': '13rem',
+            }
+        },
+    },
+    plugins: [],
+}
