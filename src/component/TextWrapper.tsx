a2c80cb58c301f1d08d9b5f57dd1c5f5dfb13cd5
kekepoid <101209551+kekepoid@users.noreply.github.com>
2022-03-09 09:35:25 +0200

    Add files via upload

--- src/component/TextWrapper.tsx
+++ src/component/TextWrapper.tsx
@@ -2,6 +2,6 @@ import {ReactNode} from "react";
 
 export function TextWrapper({children}: { children: ReactNode }) {
     return (
-        <p className={"text-xs sm:text-lg text-neutral-100 font-light leading-loose"}>{children}</p>
+        <p className={"text-xs md:text-lg text-neutral-100 font-light leading-loose"}>{children}</p>
     )
 }
